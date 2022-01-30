aefb2e2cdaf919cae33b3c3bd9f5c4b8bcbe0804
cyanChill <anthonyliang9@gmail.com>
2022-01-30 16:49:45 -0500

    quick logic to allow login on certain criterias + styling fixes

--- components/login/login.js
+++ components/login/login.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 import Button from "../form_elements/button";
 import FormInput from "../form_elements/forminput";
@@ -9,12 +10,32 @@ import classes from "./login.module.css";
 
 /* 
   Component to purely handle logins
-    - Have some custom logic to make sure that the login button is disabled if the username & password fiels are empty
+    - Have some custom logic to make sure that the login button is disabled if the username & password fiels are empty ✅
+    
+    - Min password length of 6
 */
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [canSubmit, setCanSubmit] = useState(false);
+
+  useEffect(() => {
+    if (username && password.length > 5) {
+      setCanSubmit(true);
+    } else {
+      setCanSubmit(false);
+    }
+  }, [username, password]);
+
   const loginHandler = (e) => {
     e.preventDefault();
+
+    /* 
+      Some validation
+    */
+
+    console.log(username, password);
   };
 
   return (
@@ -32,9 +53,24 @@ const Login = () => {
         </div>
 
         <form onSubmit={loginHandler}>
-          <FormInput type="text" placeholder="Username or Email" required />
-          <FormInput type="password" placeholder="Password" required />
-          <Button type="submit">Login</Button>
+          <FormInput
+            type="text"
+            placeholder="Username or Email"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FormInput
+            type="password"
+            placeholder="Password"
+            minlength="6"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" disabled={!canSubmit}>
+            Login
+          </Button>
         </form>
       </Card>
 
