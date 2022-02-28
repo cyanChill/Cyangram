76a014edc2cbca87dbd421a9a796f098e74abb90
cyanChill <anthonyliang9@gmail.com>
2022-02-27 20:52:27 -0500

    reusable navlink component - active link detection styling

---
+++ components/misc/navlink.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import classes from "./navlink.module.css";
+
+const NavLink = (props) => {
+  const router = useRouter();
+  const isActive = router.asPath == props.to;
+
+  if (isActive && props.activeEl) {
+    return (
+      <Link href={props.to} passHref>
+        <a>{props.activeEl}</a>
+      </Link>
+    );
+  }
+
+  return (
+    <Link href={props.to} passHref>
+      <a className={router.pathname == props.to ? classes.active : ""}>
+        {props.children}
+      </a>
+    </Link>
+  );
+};
+
+export default NavLink;
