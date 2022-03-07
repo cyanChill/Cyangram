f18ea51d28724c2722967ef434b4eaca5706aa92
cyanChill <anthonyliang9@gmail.com>
2022-03-07 00:12:32 -0500

    dummy post infor with home feed post structure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,22 @@
 import { getSession, signOut } from "next-auth/react";
+import HomePost from "../components/posts/homepost";
+
+const DUMMY_POST = {
+  postId: "p1",
+  uploader: {
+    username: "testUser",
+    profilePic: "https://randomuser.me/api/portraits/women/19.jpg",
+  },
+  postImg:
+    "https://d279m997dpfwgl.cloudfront.net/wp/2020/05/pencil-standardized-test.jpg",
+  /*
+  postImgs: [
+    "https://d279m997dpfwgl.cloudfront.net/wp/2020/05/pencil-standardized-test.jpg",
+  ],
+  */
+  description: "This is a test post",
+  likeCnt: 100,
+};
 
 const HomePage = (props) => {
   return (
@@ -6,6 +24,7 @@ const HomePage = (props) => {
       <div>
         <h1>This is the home feed</h1>
         <button onClick={signOut}>Sign Out</button>
+        <HomePost post={DUMMY_POST} />
       </div>
     </>
   );
