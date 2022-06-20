ec5d4109be90c6e8d30509cb2c9fd4d33397e8e6
cyanChill <anthonyliang9@gmail.com>
2022-06-20 17:30:23 -0400

    removed comment

--- pages/index.js
+++ pages/index.js
@@ -19,16 +19,12 @@ export const getServerSideProps = async (context) => {
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${session.user.username}/yourfeed`
   );
   const data = await res.json();
-  // console.log("-=-=- Feed Of People We're Following -=-=-");
-  // console.log(data.feedPosts);
 
   /* Get home feed for posts from people we don't follow (discover) */
   const res2 = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${session.user.username}/globalfeed`
   );
   const data2 = await res2.json();
-  // console.log("-=-=- Feed Of People We're Not Following -=-=-");
-  // console.log(data2.feedPosts);
 
   return {
     props: {
