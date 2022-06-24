9dac19a70007491527c004596aed7a481f0fa84d
cyanChill <anthonyliang9@gmail.com>
2022-06-24 14:48:47 -0400

    update containers to use flex instead of absolute

--- components/posts/post.js
+++ components/posts/post.js
@@ -13,25 +13,98 @@ import classes from "./post.module.css";
 
 const PostPage = ({ postData, ownPost, hasLiked, viewerId }) => {
   const postId = postData._id;
-  const username = postData.posterInfo.username;
-
-  const [commentField, setCommentField] = useState("");
-
-  const [numLikes, setNumLikes] = useState(postData.likes.length);
   const [comments, setComments] = useState(postData.comments);
-  const [postedSince, setPostedSince] = useState("");
 
-  useEffect(() => {
-    const date = postData.date;
-    setPostedSince(timeSince(date));
-  }, [postData.date]);
-
-  const focusCommentField = () => {
-    document.getElementById("commentField").focus();
+  const addComment = (comment) => {
+    setComments((prev) => [...prev, comment]);
   };
 
   const removeComment = (id) => {
     setComments(comments.filter((comment) => comment._id !== id));
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      {/* Where we put the back button & name of poster */}
+      <BackHeader
+        text={postData.posterInfo.username}
+        linkPath={`/${postData.posterInfo.username}`}
+      />
+
+      <Card className={classes.cardClasses}>
+        {/* Put the image into a container */}
+        <div className={classes.imgContainer}>
+          <LoadImage
+            src={postData.image.url}
+            alt={`Post by ${postData.posterInfo.username}`}
+            className={classes.postImg}
+            width="500"
+            height="500"
+            layout="responsive"
+            priority
+          />
+        </div>
+
+        {/* Wraps general content of post */}
+        <div className={classes.generalContent}>
+          {/* Post Description */}
+          {!!postData.description.trim() && (
+            <div className={classes.descriptionWrapper}>
+              <CommentBody
+                picUrl={postData.posterInfo.profilePic.url}
+                picAlt={`${postData.posterInfo.username} profile picture`}
+                username={postData.posterInfo.username}
+                textContent={postData.description}
+              />
+            </div>
+          )}
+
+          <CommentsContainer
+            id={postId}
+            comments={comments}
+            viewerId={viewerId}
+            removeComment={removeComment}
+          />
+
+          <ActionSection
+            id={postId}
+            date={postData.date}
+            likes={postData.likes.length}
+            ownPost={ownPost}
+            hasLiked={hasLiked}
+            addComment={addComment}
+          />
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default PostPage;
+
+const CommentsContainer = ({ id, comments, viewerId, removeComment }) => {
+  /* Div with scrollable containing comments */
+  return (
+    <div className={classes.commentContainer}>
+      {comments.map((comment) => (
+        <Comment
+          key={comment._id}
+          postId={id}
+          comment={comment}
+          viewerId={viewerId}
+          handleRemove={removeComment}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ActionSection = ({ id, date, likes, ownPost, hasLiked, addComment }) => {
+  const [numLikes, setNumLikes] = useState(likes);
+  const [commentField, setCommentField] = useState("");
+
+  const focusCommentField = () => {
+    document.getElementById("commentField").focus();
   };
 
   const handleLikes = (action) => {
@@ -54,7 +127,7 @@ const PostPage = ({ postData, ownPost, hasLiked, viewerId }) => {
     }
 
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/post/${postId}/comment`,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/post/${id}/comment`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -70,107 +143,52 @@ const PostPage = ({ postData, ownPost, hasLiked, viewerId }) => {
 
     // If we successfully created comment, update client-side
     if (res.ok) {
-      setComments((prev) => [...prev, data.comment]);
+      addComment(data.comment);
       setCommentField("");
     }
   };
 
   return (
-    <div className={classes.wrapper}>
-      {/* Where we put the back button & name of poster */}
-      <BackHeader text={username} linkPath={`/${username}`} />
+    <div>
+      {/* Like button, "comment, share" buttons, settings drop down */}
+      <PostActions
+        postId={id}
+        settings
+        likeBtnAction={handleLikes}
+        commentBtnAction={focusCommentField}
+        isOwner={ownPost}
+        hasLiked={hasLiked}
+      />
 
-      <Card className={classes.cardClasses}>
-        {/* Put the image into a container */}
-        <div className={classes.imgContainer}>
-          <LoadImage
-            src={postData.image.url}
-            alt={`Post by ${username}`}
-            className={classes.postImg}
-            width="500"
-            height="500"
-            layout="responsive"
-            priority
-          />
-        </div>
+      <div className={classes.statistics}>
+        <p className={classes.likeCount}>{numLikes} Likes</p>
+        <p className={classes.postedSince}>{timeSince(date)}</p>
+      </div>
 
-        {/* Wraps general content of post */}
-        <div className={classes.generalContent}>
-          <div className={classes.nonActions}>
-            {/* Post Description */}
-            {!!postData.description.trim() && (
-              <div className={classes.descriptionWrapper}>
-                <CommentBody
-                  picUrl={postData.posterInfo.profilePic.url}
-                  picAlt={`${postData.posterInfo.username} profile picture`}
-                  username={postData.posterInfo.username}
-                  textContent={postData.description}
-                />
-              </div>
-            )}
+      {/* Comment field */}
+      <form onSubmit={handleCommentSubmit} className={classes.commentField}>
+        <FormInput
+          id="commentField"
+          type="text"
+          placeholder="Add a comment..."
+          maxLength="200"
+          required
+          value={commentField}
+          onChange={(e) => setCommentField(e.target.value.trimStart())}
+          onBlur={() => setCommentField((prev) => prev.trimEnd())}
+          noExternalPadding
+        />
 
-            {/* Div with scrollable containing comments */}
-            <div className={classes.commentContainer}>
-              {comments.map((comment) => (
-                <Comment
-                  key={comment._id}
-                  postId={postId}
-                  comment={comment}
-                  viewerId={viewerId}
-                  handleRemove={removeComment}
-                />
-              ))}
-            </div>
-          </div>
-
-          <div className={classes.action}>
-            {/* Like button, "comment, share" buttons, settings drop down */}
-            <PostActions
-              postId={postId}
-              settings
-              likeBtnAction={handleLikes}
-              commentBtnAction={focusCommentField}
-              isOwner={ownPost}
-              hasLiked={hasLiked}
-            />
-
-            <div className={classes.statistics}>
-              <p className={classes.likeCount}>{numLikes} Likes</p>
-              <p className={classes.postedSince}>{postedSince}</p>
-            </div>
-
-            {/* Comment field */}
-            <form
-              onSubmit={handleCommentSubmit}
-              className={classes.commentField}
-            >
-              <FormInput
-                id="commentField"
-                type="text"
-                placeholder="Add a comment..."
-                maxLength="200"
-                required
-                value={commentField}
-                onChange={(e) => setCommentField(e.target.value.trimStart())}
-                onBlur={() => setCommentField((prev) => prev.trimEnd())}
-                noExternalPadding
-              />
-
-              {/* Disable the following if the textfield is empty */}
-              <span
-                variant="clear"
-                className={classes.postBtn}
-                disabled={!commentField && commentField.length <= 200}
-                onClick={handleCommentSubmit}
-              >
-                Post
-              </span>
-            </form>
-          </div>
-        </div>
-      </Card>
+        {/* Disable the following if the textfield is empty */}
+        <span
+          variant="clear"
+          className={classes.postBtn}
+          disabled={!commentField && commentField.length <= 200}
+          onClick={handleCommentSubmit}
+        >
+          Post
+        </span>
+      </form>
     </div>
   );
 };
-
-export default PostPage;
